2573b75c9fdeaf3d82f44b60a3c4622a7fd80e9f
Fareez Mir <45440971+fareezmir@users.noreply.github.com>
2025-01-25 21:09:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,40 @@ const  weatherRes = document.getElementById("weather-res")
 const  formEl = document.getElementById("weather-form")
 
 
-formEl.addEventListener("submit", function() {
+fetchWeatherData();
+
+async function fetchWeatherData(location) {
+    const APIKey = "2e5c6bdcc1804427a4514717252601";
+    const APIUrl = `http://api.weatherapi.com/v1/forecast.json?key=${APIKey}&q=${location}&days=7`;
+
+    try {
+        const response = await fetch(APIUrl);
+        if (!response.ok) {
+            throw new Error("Location was not found.")
+        }
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        throw error;
+    }
+}
+formEl.addEventListener("submit", function(event) {
+    weatherRes.textContent = "";
+    event.preventDefault(); //removes the refresh after putting input
+    const location = document.getElementById("user-location").value;
+    if (!location.trim()) {
+        weatherRes.textContent = "Please enter a valid location";
+    }
+    else {
+        try {
+            const data = fetchWeatherData(location);
+        }
+        catch {
+            console.error(error);
+
+        }
+    }
     
 })
+
