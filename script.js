f96d103beca91a63e353d28b25e764b8ef81c448
Fareez Mir <45440971+fareezmir@users.noreply.github.com>
2025-01-25 23:35:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const  weatherRes = document.getElementById("weather-res")
+const  currentWeather = document.getElementById("current-weather")
 const  formEl = document.getElementById("weather-form")
+const  tempEl = document.getElementById("temp-el")
+const  cityEl = document.getElementById("city-name-el")
+const  feelslikeEl = document.getElementById("feels-like-el")
+const  humidityEl = document.getElementById("humidity-el")
+const  errorEl = document.getElementById("error-el")
 
-
-fetchWeatherData();
 
 async function fetchWeatherData(location) {
     const APIKey = "2e5c6bdcc1804427a4514717252601";
-    const APIUrl = `http://api.weatherapi.com/v1/forecast.json?key=${APIKey}&q=${location}&days=7`;
+    const APIUrl = `https://api.weatherapi.com/v1/forecast.json?key=${APIKey}&q=${location}&days=7`;
 
     try {
         const response = await fetch(APIUrl);
@@ -14,26 +17,67 @@ async function fetchWeatherData(location) {
             throw new Error("Location was not found.")
         }
         const data = await response.json();
+        console.log(data)
         return data;
     }
     catch (error) {
         throw error;
     }
 }
-formEl.addEventListener("submit", function(event) {
-    weatherRes.textContent = "";
+
+function displayWeatherInfo(data) {
+    const {
+        location: {name}, current: {temp_c, feelslike_c, humidity, condition:{text, icon}}
+    } = data;
+
+    //Current Day Info
+    cityEl.textContent = name;
+    tempEl.textContent = temp_c;
+    feelslikeEl.textContent = feelslike_c;
+    humidityEl.textContent = humidity;
+
+    //Forecast
+    displayForcast(data)
+}
+
+function displayForcast(data) {
+    const {
+        forecast: {forecastday}
+    } = data;
+
+    forecastday.forEach((day) => {
+        const {date, day: {maxtemp_c, mintemp_c}, condition: {text, icon}} = day;
+
+        const forecastDay = document.createElement("div");
+        forecastDay.classList.add("forecast-item")
+        forecastDay.innerHTML = `
+        <h3>${date}<\h3>
+        <p>Max Temp: ${maxtemp_c} <\p>
+        <p> Min Temp: ${mintemp_c} <\p>
+        <p> Condition: ${text} <\p>
+        `
+
+    });
+}
+
+function displayError() {
+    errorEl.textContent = "Please enter a valid location"
+}
+
+formEl.addEventListener("submit", async function(event) {
     event.preventDefault(); //removes the refresh after putting input
     const location = document.getElementById("user-location").value;
+    
     if (!location.trim()) {
-        weatherRes.textContent = "Please enter a valid location";
+        displayError()
     }
     else {
         try {
-            const data = fetchWeatherData(location);
+            const data = await fetchWeatherData(location);
+            displayWeatherInfo(data);
         }
-        catch {
-            console.error(error);
-
+        catch (error) {
+            console.error(error.message);
         }
     }
     
